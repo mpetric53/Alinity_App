c05e3cb1d752709688a99b62dbabf9a3f29dce3a
Mislav Rukonic <mislavrukonic@gmail.com>
2021-04-11 20:41:30 +0200

    Update SQL script. Update User class with comments and change Main class to accept user input for where they want to connect to, and change order of code. Remove original SQL file due to commit issues.

--- Database_Project_Group1/src/AlinityMain.java
+++ Database_Project_Group1/src/AlinityMain.java
@@ -1,11 +1,53 @@
+import java.util.Scanner;
+
 public class AlinityMain {
 
     public static Alinity alinityDB = null;
 
     public static void main(String[] args) {
         try {
-            alinityDB = new Alinity("mysql", "localHost", "3306", "alinity", "root", "Marlenagang4519995711121999robot123-");
+            /**
+             * Scanner object to take in user input about the database.
+             * Based on this, the database connection either will or
+             * will not be established.
+             */
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter name of DBMS you wish to use (i.e. 'mysql'): ");
+            String dbms = scan.nextLine();
+            System.out.print("Enter server name (i.e. 'localHost'): ");
+            String serverName = scan.nextLine();
+            System.out.print("Enter port number: ");
+            String portNumber = scan.nextLine();
+            System.out.print("Enter the name of the DB you wish to connect to: ");
+            String dbName = scan.nextLine();
+            System.out.print("Enter your username: ");
+            String username = scan.nextLine();
+            System.out.print("Enter your password: ");
+            String password = scan.nextLine();
+
+            /**
+             * The object should have the following set in its "creation" (all are Strings):
+             * dbms (name of database management system, such as MySQL), serverName, portNumber, dbName (name of
+             * the database you wish to connect to), username, and password.
+             *
+             */
+            alinityDB = new Alinity(dbms, serverName, portNumber, dbName, username, password);
             alinityDB.connect();
+
+            /**
+             * attempts to login with the information the
+             * user has presented. If the user has failed,
+             * ask again up to a total of three times.
+             */
+            User user = new User();
+            System.out.println("Enter your username and password: ");
+            String userId = scan.nextLine();
+            String passwordId = scan.nextLine();
+
+            while (!user.login(userId, passwordId)) {
+                userId = scan.nextLine();
+                passwordId = scan.nextLine();
+            }
         } catch (AlinityException ae) {
             System.out.println("Test error!");
         } finally {
--- Database_Project_Group1/src/User.java
+++ Database_Project_Group1/src/User.java
@@ -8,16 +8,21 @@ public class User {
     private String password;
     private Date birthday;
     private String email;
+    private String role;
+    private int counter = 0;
+    private final int MAX_ATTEMPTS = 3;
 
-    public User(int userId, String username, String password, Date birthday, String email){
+    public User(int userId, String username, String password, Date birthday, String email, String role){
         this.userId = userId;
         this.username = username;
         this.password = password;
         this.birthday = birthday;
         this.email = email;
+        this.role = role;
     }
 
     public User() {
+
     }
 
     public void setUserId(int userId) {
@@ -40,6 +45,91 @@ public class User {
         this.email = email;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    /**
+     * authenticate method of the User class.
+     * Will attempt to authenticate the users login information
+     * by checking if the input information is in the DB or not.
+     * If successful, that user and its "object" will be set.
+     * This is important, since we use the user's ID to retrieve
+     * their specific saved songs, albums, and artists.
+     *
+     * @param username
+     * @param password
+     * @return
+     * @throws AlinityException
+     */
+    public boolean authenticate(String username, String password) throws AlinityException{
+        try{
+            ArrayList<String> credentials = new ArrayList<>();
+            credentials.add(username);
+            credentials.add(password);
+            ArrayList<ArrayList<String>> result = AlinityMain.alinityDB.getData("SELECT * FROM User WHERE User.id = ? AND User.password = ?" , credentials);
+            if(result.size() <= 1) {
+                System.out.println("Invalid credentials.");
+                return false;
+            }
+            ArrayList<String> userData = result.get(1);
+            setUserId(Integer.parseInt(userData.get(0)));
+            setUsername(userData.get(1));
+            setPassword(userData.get(2));
+            setBirthday(Date.valueOf(userData.get(3)));
+            setEmail(userData.get(4));
+            setRole(userData.get(5));
+            return true;
+        } catch (IndexOutOfBoundsException ioobe) {
+            throw new AlinityException(ioobe, "-> Error authenticating.", null);
+        }
+    }
+
+    /**
+     * login method of the User class.
+     * Will allow a user to login and use our system. This is done
+     * by calling the authenticate method to check their information.
+     * If the information is incorrect, the user will have 2 more tries
+     * to input valid information. Else, it will authenticate properly,
+     * and if the user is an admin, it will display their information.
+     *
+     * @param username
+     * @param password
+     * @return
+     * @throws AlinityException
+     */
+    public boolean login(String username, String password) throws AlinityException {
+        if(!authenticate(username, password)) {
+            counter++;
+            if(counter >= MAX_ATTEMPTS) {
+                System.out.println("Too many incorrect attempts, terminating system...");
+                System.exit(0);
+            }
+            System.out.println("Incorrect credentials, try again. You have: " + (MAX_ATTEMPTS - counter) + " more tries");
+            return false;
+        } else {
+            authenticate(username, password);
+            if(this.getRole().equals("Admin")) {
+                printInfo();
+            }
+            return true;
+        }
+    }
+
+    public void printInfo() {
+        System.out.println("\nUser ID: " + this.userId);
+        System.out.println("Username: " + this.username);
+        System.out.println("User password: " + this.password);
+        System.out.println("User birthday: " + this.birthday);
+        System.out.println("User email: " + this.email);
+        System.out.println("User role: " + this.role);
+    }
+
     /**
      * selectAll method of the User class.
      * This method will use the getData(String, ArrayList<String>) method of the Alinity
@@ -53,15 +143,16 @@ public class User {
      */
     public void selectAll(ArrayList<String> stringList) throws AlinityException {
         try {
-            ArrayList<ArrayList<String>> result = AlinityMain.alinityDB.getData("SELECT * FROM User WHERE userId = ?" , stringList);
-            System.out.print("\nColumn headers: " + result.get(0));
-            ArrayList<String> userData = result.get(1);
-            setUserId(Integer.parseInt(userData.get(0)));
-            setUsername(userData.get(1));
-            setPassword(userData.get(2));
-            setBirthday(Date.valueOf(userData.get(3)));
-            setEmail(userData.get(4));
-
+            if(this.getRole().equals("Admin")) {
+                ArrayList<ArrayList<String>> result = AlinityMain.alinityDB.getData("SELECT * FROM User WHERE userId = ?", stringList);
+                System.out.print("\nColumn headers: " + result.get(0));
+                ArrayList<String> userData = result.get(1);
+                setUserId(Integer.parseInt(userData.get(0)));
+                setUsername(userData.get(1));
+                setPassword(userData.get(2));
+                setBirthday(Date.valueOf(userData.get(3)));
+                setEmail(userData.get(4));
+            } else System.out.println("You do not have the right permissions to perform this action!");
         } catch (IndexOutOfBoundsException ioobe) {
             throw new AlinityException(ioobe, "-> Error in obtaining data (IndexOutOfBoundsException) from the database. Please check your syntax in the selectAll(ArrayList<String>) method.", "SELECT * FROM User WHERE userId = ?");
         } catch (NullPointerException npe) {
