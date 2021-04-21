c1041075a5c50cae4455ecdd7e0e03bf645c54b7
MatejPetric <mp2272@g.rit.edu>
2021-04-21 21:20:55 +0200

    Controller added with the action events linked to the view buttons, added getters and setters for all GUI inputs

--- Database_Project_Group1/src/AlinityController.java
+++ Database_Project_Group1/src/AlinityController.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+
 public class AlinityController {
    private LogInGUI login = new LogInGUI();
    private ViewGUI view = new ViewGUI();
@@ -12,6 +14,43 @@ public class AlinityController {
 
 
     public void start(){
-        //this.view.getjButton1().addActionListener();
+
+        //Action Listener for the button LogInGUI class
+        this.login.getjButton1().addActionListener((ActionEvent e) -> {
+            System.out.println("--- LOGIN BUTTON ---");
+        });
+
+
+        //Action Listener for the button SignUpGUI class
+        this.signup.getjButton1().addActionListener((ActionEvent e) -> {
+            System.out.println("--- SIGNUP BUTTON ---");
+        });
+
+
+        //Action Listener for the button ViewGUI class
+        this.view.getjButton1().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 1 ---");
+        });
+        this.view.getjButton2().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 2 ---");
+        });
+        this.view.getjButton3().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 3 ---");
+        });
+        this.view.getjButton4().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 4 ---");
+        });
+        this.view.getjButton5().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 5 ---");
+        });
+        this.view.getjButton6().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 6 ---");
+        });
+        this.view.getjButton7().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 7 ---");
+        });
+        this.view.getjButton8().addActionListener((ActionEvent e) -> {
+            System.out.println("--- VIEW BUTTON 8 ---");
+        });
     }
 }
